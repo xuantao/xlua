ea186ff65be3c64fc1d27ddbd46b6195dbeb6d37
xuantao <xuantao.zh@gmail.com>
2019-11-13 20:50:18 +0800

    add gtest lib

--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,30 @@
 ﻿#include "lua_export.h"
 #include "test.h"
+#include "gtest/gtest.h"
 #include <stdio.h>
 #include <xlua.h>
 
+TEST(first_test, test_1) {
 
-int main(int argc, char* argvp[]) {
+}
+
+int main(int argc, char* argv[]) {
 //    xlua::Startup(nullptr);
-    xlua::State* l = xlua::CreateState(nullptr);
+    //xlua::State* l = xlua::CreateState(nullptr);
 
-    l->LoadGlobal("_G");
-    assert(xlua::VarType::kTable == l->GetType(-1));
-    l->PopTop(1);
+    //l->LoadGlobal("_G");
+    //assert(xlua::VarType::kTable == l->GetType(-1));
+    //l->PopTop(1);
 
-    TestPushLoad(l);
-    TestExport(l);
-    TestGlobal(l);
+    //TestPushLoad(l);
+    //TestExport(l);
+    //TestGlobal(l);
 
-    Triangle* triangle = nullptr;
-    l->Push(triangle);
+    //Triangle* triangle = nullptr;
+    //l->Push(triangle);
 
-    //l->Release();
-    xlua::DestoryState(l);
-    return 0;
+    ////l->Release();
+    //xlua::DestoryState(l);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
